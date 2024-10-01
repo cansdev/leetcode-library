47eed07d6c74716d3a4985f416ad94cd081b449b
canss <ozcan02xl@gmail.com>
2024-10-01 19:05:22 +0300

    updating longestCommonPrefix

--- problem-solutions/easy/longestCommonPrefix.cpp
+++ problem-solutions/easy/longestCommonPrefix.cpp
@@ -3,8 +3,8 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string prefix = strs[0];
         for(int i=0; i<strs.size(); i++) {
-            while(strs[i].find(prefix) != 0) {
-                prefix = prefix.substr(0, prefix.size() -1);
+            while(strs[i].find(prefix)) {
+                prefix.pop_back();
                 if (prefix.empty())
                     return "";
             }
