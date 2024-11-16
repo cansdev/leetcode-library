a11fca856cd64b600dace2aeec6bee3269a7d537
canss <ozcan02xl@gmail.com>
2024-11-16 22:03:58 +0300

    pushing studentsAndExaminations

---
+++ problem-solutions/sql/easy/studentsAndExaminations.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+select s.student_id, s.student_name, subj.subject_name, count(e.subject_name) as attended_exams
+from Students s
+cross join Subjects subj
+left join Examinations e on s.student_id = e.student_id
+and subj.subject_name = e.subject_name 
+group by s.student_id, s.student_name, subj.subject_name
+order by s.student_id, subj.subject_name
